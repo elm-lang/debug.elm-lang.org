5870dbd8e401cad516554700d3c270db341f4cad
Evan Czaplicki <info@elm-lang.org>
2014-03-31 22:28:59 -0700

    Start experimenting with short embedded videos

--- public/Home.elm
+++ public/Home.elm
@@ -18,15 +18,12 @@ padCol w col =
 scene w =
     flow down 
     [ spacer w 20
-    , flow right [ width 640 intro
-                 , tile (max 300 (w - 640)) examples
-                 ]
+    , width 640 intro
+    , tile 640 examples
     ]
 
 examples : [[String]]
-examples = [ [ "Mario", "TextReverse"]
-           , [ "Mario", "TextReverse"]
-           , [ "Mario", "TextReverse"]
+examples = [ [ "Stamps", "TextReverse", "Mario" ]
            ]
 
 intro = [markdown|
@@ -42,6 +39,11 @@ p, li {
 </style>
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum porta orci, sed elementum mauris laoreet tristique. Curabitur vehicula ipsum sed dolor tincidunt, at pharetra velit egestas. Aenean pellentesque at tellus nec placerat. Donec vehicula libero in luctus ornare. Aliquam faucibus, risus sit amet lacinia faucibus, est ante consectetur erat, ullamcorper tristique mi orci nec nibh. Duis dapibus, neque quis blandit mollis, leo felis imperdiet augue, sit amet lacinia sapien tellus sit amet tellus. Mauris consequat posuere quam, id ultrices sapien fermentum ac. Curabitur ante elit, molestie in urna ac, commodo viverra leo. Aliquam blandit, arcu vel consequat fermentum, eros nunc ultricies nisl, mollis sollicitudin velit lacus at odio. Suspendisse potenti.
+
+<video width="640" height="132" preload controls>
+  <source src="/videos/mario.mp4" type="video/mp4">
+  <source src="/videos/mario.webm" type="video/webm">
+</video>
 
 <iframe width="640"
         height="360"
