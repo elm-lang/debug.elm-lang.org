0cb89c6c46d551d71fc04e8f95ef2e4bd62540b9
Evan Czaplicki <info@elm-lang.org>
2014-03-31 23:40:31 -0700

    Make example clearer and thinner

--- examples/Intermediate/Stamps.elm
+++ examples/Intermediate/Stamps.elm
@@ -2,15 +2,20 @@
 import Mouse
 import Window
 
+main : Signal Element
 main = lift2 scene Window.dimensions clickLocations
 
--- for a good time, remove "sampleOn Mouse.clicks" ;)
+clickLocations : Signal [(Int,Int)]
 clickLocations = foldp (::) [] (sampleOn Mouse.clicks Mouse.position)
 
+scene : (Int,Int) -> [(Int,Int)] -> Element
 scene (w,h) locs =
   let drawPentagon (x,y) =
-          ngon 5 20 |> filled (hsva (toFloat x) 1 1 0.7)
-                    |> move (toFloat x - toFloat w / 2, toFloat h / 2 - toFloat y)
-                    |> rotate (toFloat x)
-  in  layers [ collage w h (map drawPentagon locs)
-             , plainText "Click to stamp a pentagon." ]
+          ngon 5 20
+            |> filled (hsva (toFloat x) 1 1 0.7)
+            |> move (toFloat x - toFloat w/2, toFloat h/2 - toFloat y)
+            |> rotate (toFloat x)
+  in
+      layers [ collage w h (map drawPentagon locs)
+             , plainText "Click to stamp a pentagon."
+             ]
