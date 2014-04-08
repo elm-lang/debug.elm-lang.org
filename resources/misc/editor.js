368ffb792a450bfb4ec63a04154c093e11bd9023
Evan Czaplicki <info@elm-lang.org>
2014-04-07 20:44:15 -0700

    auto swap is on by default

--- resources/misc/editor.js
+++ resources/misc/editor.js
@@ -400,11 +400,15 @@ function createCookie(name,value,days) {
 
 function readCookie(name) {
   var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+  var cookies = document.cookie.split(';');
+  for(var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+        cookie = cookie.substring(1,cookie.length);
+    }
+    if (cookie.indexOf(nameEQ) === 0) {
+        return cookie.substring(nameEQ.length,cookie.length);
+    }
   }
   return null;
 }
@@ -448,12 +452,9 @@ function initMenu() {
   showType(show);
 }
 
-function isBounceInIFrame() {
-    return window.location.pathname.indexOf("Bounce") >= 0 && window !== window.top;
-}
-
 function initAutoHotSwap() {
-    var status = readCookie('autoHotSwap') === 'true' || isBounceInIFrame();
+    var cookie = readCookie('autoHotSwap');
+    var status = true; // cookie === 'true' || cookie === null;
     document.getElementById('auto_hot_swap_checkbox').checked = status;
     setAutoHotSwap(status, true);
 }
@@ -476,7 +477,6 @@ function initEditor() {
               }
           }
         });
-    if (!isBounceInIFrame()) editor.focus();
     editor.on('cursorActivity', hideStuff);
     initZoom();
     initMenu();
