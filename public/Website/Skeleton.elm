46323b88c788ac5530a083e0bcb154af341370b7
Evan Czaplicki <info@elm-lang.org>
2014-04-14 15:19:24 -0700

    Improve the subtitle

--- public/Website/Skeleton.elm
+++ public/Website/Skeleton.elm
@@ -74,7 +74,7 @@ title =
     [ link "/" <| leftAligned <| bigWords
     , spacer 10 4
     , leftAligned . Text.height 16 . typeface faces . Text.color C.mediumGrey <|
-          toText "Control time and change history with Elm&rsquo;s new debugger."
+          toText "Pause, rewind, and replay any program. Debug by changing history."
     ]
 
 footer outer = container outer footerHeight footerPosition <| Text.centered footerWords
