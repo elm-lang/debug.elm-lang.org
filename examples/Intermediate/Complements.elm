eef64336ae9289e23e9709976264b2bcde3b29a1
Evan Czaplicki <info@elm-lang.org>
2014-05-25 13:28:08 -0700

    Remove a ton of unused examples

--- examples/Intermediate/Complements.elm
+++
@@ -1,8 +0,0 @@
- 
-main = lift colorCycle (foldp (+) 0 (fps 30))
-
-colorCycle time =
-  let toDot angle = circle (20 + 10 * sin (angle * 2 + inSeconds time))
-                      |> filled (hsv angle 0.9 0.9)
-                      |> move (fromPolar (100, angle + pi))
-  in  collage 300 300 (map (\n -> toDot <| turns (n/12)) [0..11])
